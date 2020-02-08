0092a59fcca05fc4b41cebc35fc4a0a010e5e121
Min Thaw Zin <minthaw.zin11@gmail.com>
2020-02-08 09:41:30 +0630

    VERIFY_TOKEN KEYWORD

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('1689363891')
+	res.send('HELLO I AM LEARNING!')
 })
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['VERIFY_TOKEN'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
