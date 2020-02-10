2e289b80bdd368a1a795d60efc3181cbe74ca342
Min Thaw Zin <minthaw.zin11@gmail.com>
2020-02-10 12:35:30 +0630

    Bot speaking

--- index.js
+++ index.js
@@ -30,3 +30,37 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAADVD6TjNt0BAP4fl3Ciy0UU8bJo2aZCa7bcN58jTniyQdr5ptxZBv32pY3vbXqLU1YJTUZCixlvZCsg8O088LwGH5bfZCYt5fdZAS06m6dt3ksXNa1idXbZAb28OFjlmeLRV96OReAWAQ4JkzYBO4DVxLTy5nBFJKG0uFwtj44iMRpVLj54g3ZC68QufP6TvFsZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
