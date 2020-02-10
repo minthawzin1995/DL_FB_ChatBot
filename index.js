8478f0b6b72e31818c1b8f6e105df7e6422b516c
Min Thaw Zin <minthaw.zin11@gmail.com>
2020-02-10 14:01:29 +0630

    Updated bot to speak functionality

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict'
 
+// constants used in this project file
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+// setting the port to the environment or 5000
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -13,12 +15,16 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Process application/json
 app.use(bodyParser.json())
 
-// Index route
+// Index route sendin the following message
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
+/* for Facebook verification
+ * Setting up webhook
+ * "token_verification" == verfication in webhook
+ * Url of webhook => heroku server URL
+*/
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'token_verification') {
 		res.send(req.query['hub.challenge'])
@@ -26,11 +32,16 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
-// Spin up the server
+// Listening to the server requests at the specified port
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
 
+/* Post function on the webhook/
+ * get the messages from the other user
+ * reply back the same message with the Text received echo:
+ * send server 200 code if passed
+*/
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
@@ -44,12 +55,15 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-const token = "EAADVD6TjNt0BAP4fl3Ciy0UU8bJo2aZCa7bcN58jTniyQdr5ptxZBv32pY3vbXqLU1YJTUZCixlvZCsg8O088LwGH5bfZCYt5fdZAS06m6dt3ksXNa1idXbZAb28OFjlmeLRV96OReAWAQ4JkzYBO4DVxLTy5nBFJKG0uFwtj44iMRpVLj54g3ZC68QufP6TvFsZD"
-
+/* Send Text Message method
+ * Use of v6.0 updated facebook graph API
+ * access_token is configured through heroku server
+ * heroku config: set FB_PAGE_ACCESS_TOKEN = <>
+*/
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    url: 'https://graph.facebook.com/v6.0/me/messages',
 	    qs: {access_token:token},
 	    method: 'POST',
 		json: {
@@ -63,4 +77,3 @@ function sendTextMessage(sender, text) {
 		    console.log('Error: ', response.body.error)
 	    }
     })
-}
