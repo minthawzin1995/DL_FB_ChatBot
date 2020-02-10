130821d2d0c56eca330105573e55970a3e13d6ba
Min Thaw Zin <minthaw.zin11@gmail.com>
2020-02-10 11:45:55 +0630

    Default

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//This is still work in progress
+/*
+Please report any bugs to nicomwaks@gmail.com
+i have added console.log on line 48
+ */
 'use strict'
 
 const express = require('express')
@@ -7,108 +12,125 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-// Process application/x-www-form-urlencoded
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
-// Process application/json
+// parse application/json
 app.use(bodyParser.json())
 
-// Index route
+// index
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+	res.send('hello world i am a secret bot')
 })
 
-// for Facebook verification
+// for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
+	} else {
+		res.send('Error, wrong token')
 	}
-	res.send('Error, wrong token')
 })
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
-/*
+// to post data
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i]
-	    let sender = event.sender.id
-	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-	    }
-    }
-    res.sendStatus(200)
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			if (text === 'Generic'){
+				console.log("welcome to chatbot")
+				//sendGenericMessage(sender)
+				continue
+			}
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+		if (event.postback) {
+			let text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			continue
+		}
+	}
+	res.sendStatus(200)
 })
-*/
 
-const token = "EAADVD6TjNt0BAFMCp7k8Y1FTjnzCCfNCWBmH2VdKtduDtEi6uco96Rm9EaZCDSMMGNUwh2aSPZCohlzo2rVL92fSlx4y3kQLaOZCZBSuKlLIGh0LWLjivXrZApEZAFH1sTHPnKMZC4AyI3412FhPSbGx2sMD4RjAEUZB0JtZA9XuEPR7I3W4Av1e5aQco51jprjxK2ov0AY3Y41zKReoNQe7c"
+
+// recommended to inject access tokens as environmental variables, e.g.
+// const token = process.env.FB_PAGE_ACCESS_TOKEN
+const token = "EAADVD6TjNt0BAJ8EL5NmsnRdQkjPRPH7QosZAJrIdPWtA479m7Hm047j3Xe5StvTlAlJLZBrbqx4CR8lI02BUbRMSfeKZBe7LfaZCK8Vbz0YKuKUBIEkPVIiVA1znEpLfX0IMNJwpHbrP7j1hdGaGV6uglCWU05LyCCsSITIHXnyatAN8DGc5FqdBPAMKvkZD"
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-	    url: 'https://graph.facebook.com/v6.0/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
+	let messageData = { text:text }
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
 		json: {
-		    recipient: {id:sender},
-				message: messageData,
+			recipient: {id:sender},
+			message: messageData,
 		}
 	}, function(error, response, body) {
 		if (error) {
-		    console.log('Error sending messages: ', error)
+			console.log('Error sending messages: ', error)
 		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
+			console.log('Error: ', response.body.error)
+		}
+	})
 }
 
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
+function sendGenericMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
 }
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-
-}
-
-app.post('/webhook', (req, res) => {
-
-  // Parse the request body from the POST
-  let body = req.body;
-
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-		body.entry.forEach(function(entry) {
-
-		  // Gets the body of the webhook event
-		  let webhook_event = entry.messaging[0];
-		  console.log(webhook_event);
-
-		  // Get the sender PSID
-		  let sender_psid = webhook_event.sender.id;
-		  console.log('Sender PSID: ' + sender_psid);
-
-		});
-
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
+// spin spin sugar
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
