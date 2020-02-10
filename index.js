dfd901ff6de2d8fd170f320eda0be331804e8899
Min Thaw Zin <minthaw.zin11@gmail.com>
2020-02-10 14:07:03 +0630

    change version

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook/', function (req, res) {
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
-	    url: 'https://graph.facebook.com/v6.0/me/messages',
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
 	    qs: {access_token:token},
 	    method: 'POST',
 		json: {
